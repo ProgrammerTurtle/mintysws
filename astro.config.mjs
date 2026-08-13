6d7178b5edcbe4c4865f249f5d37c566fe471f04
Parker Rupe <97370085+ProgrammerTurtle@users.noreply.github.com>
2026-08-13 23:57:30 +0000

    make it work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://programmerturtle.github.io/mint',
-  base: '/mintysws'
+  site: 'https://programmerturtle.github.io',
+  base: '/mintysws',
 });
