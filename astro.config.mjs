7c9cd0929dad7f88d85ca4845a0e3c3ec5bb400d
lordbagel <raygenrrupe@gmail.com>
2026-08-13 17:50:24 -0600

    fix: configure GitHub Pages site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://program​​merturtle.github.io',
+  site: 'https://programmmerturtle.github.io',
   base: '/mintysws'
 });
